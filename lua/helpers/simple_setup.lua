9e7c4975ce9f366c35379a553430bfcc4d7c3a67
Stattek <stattek1@pm.me>
2026-06-03 22:49:16 -0500

    Update module

--- lua/helpers/simple_setup.lua
+++ lua/helpers/simple_setup.lua
@@ -1,11 +1,13 @@
-local SIMPLE_SETUP = {}
+---@module Simple setup functions for Neovim's vim.pack plugin manager.
+
+local M = {}
 
 ---Adds a simple package to vim.pack.
 ---@param repo string
 ---@param package_name string
-function SIMPLE_SETUP.vim_pack_add_simple(repo, package_name)
+function M.vim_pack_add_simple(repo, package_name)
   vim.pack.add { repo }
   require(package_name).setup {}
 end
 
-return SIMPLE_SETUP
+return M
